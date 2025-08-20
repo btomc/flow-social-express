77d968d6e38a75811f92b948286a24d72a75b0fb
Beata Tomczak <tma.btty@gmail.com>
2025-08-21 00:49:20 +0200

    Feed page - recent messages - ui

--- client/src/components/StoriesBar.jsx
+++ client/src/components/StoriesBar.jsx
@@ -19,7 +19,7 @@ const StoriesBar = () => {
   }, [])
 
   return (
-    <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl no-scrollbar overflow-x-auto px-4'>
+    <div className='w-screen sm:w-[calc(100vw-240px)] lg:max-w-2xl non-scrollbar overflow-x-auto px-4'>
       <div className='flex gap-4 pb-5'>
         {/* Add Story Card */}
         <div
