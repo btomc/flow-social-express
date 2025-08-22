a2e5b6bb483dc30ca47e88ad7e8e93e1690b8e2c
Beata Tomczak <tma.btty@gmail.com>
2025-08-22 23:05:22 +0200

    Create user api routes - backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,10 +11,18 @@ import CreatePost from './pages/CreatePost'
 import Layout from './pages/Layout'
 import { Toaster } from 'react-hot-toast'
 
-import { useUser } from '@clerk/clerk-react'
+import { useUser, useAuth } from '@clerk/clerk-react'
+import { useEffect } from 'react'
 
 const App = () => {
   const { user } = useUser()
+  // const { getToken } = useAuth()
+
+  // useEffect(() => {
+  //   if (user) {
+  //     getToken().then((token) => console.log(token))
+  //   }
+  // }, [user])
 
   return (
     <>
--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,12 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema(
   {
     _id: { type: String, required: true },
     email: { type: String, required: true },
     full_name: { type: String, required: true },
-    username: { type: String, required: true },
+    username: { type: String, unique: true },
+    bio: { type: String, default: 'Hey there! I am using Flow.' },
     profile_picture: { type: String, default: '' },
     cover_photo: { type: String, default: '' },
     location: { type: String, default: '' },
