f9a1d6d56a39eff96c9f2d4600201ab15a13d39a
Beata Tomczak <tma.btty@gmail.com>
2025-08-22 12:44:55 +0200

    Connect to db - mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
 
 const app = express()
+
+await connectDB()
 
 app.use(express.json())
 app.use(cors())
