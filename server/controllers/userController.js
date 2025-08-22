316138355d7dbf4e0444143552075382f3ebbc41
Beata Tomczak <tma.btty@gmail.com>
2025-08-22 21:37:16 +0200

    File upload & save images in db - backend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
+import imagekit from '../config/imageKit.js'
 import User from '../models/User.js'
+import fs from 'fs'
 
 // Get User Data using userId
 export const getUserData = async (req, res) => {
@@ -42,6 +44,48 @@ export const updateUserData = async (req, res) => {
 
     const profile = req.files.profile && req.files.profile[0]
     const cover = req.files.cover && req.files.cover[0]
+
+    // Profile image
+    if (profile) {
+      const buffer = fs.readFileSync(profile.path)
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      })
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: 'auto' },
+          { format: 'webp' },
+          { width: '512' },
+        ],
+      })
+      updatedData.profile_picture = url
+    }
+
+    // Cover image
+    if (cover) {
+      const buffer = fs.readFileSync(cover.path)
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      })
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: 'auto' },
+          { format: 'webp' },
+          { width: '1280' },
+        ],
+      })
+      updatedData.cover_photo = url
+    }
+
+    const user = await User.findByIdAndUpdate(userId, updatedData, {
+      new: true,
+    })
+
+    res.json({ success: true, user, message: 'Profile updated successfully' })
   } catch (error) {
     console.log(error)
     res.json({ success: false, message: error.message })
