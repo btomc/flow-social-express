a9b32f46b385772b1c7a7499e2ffb3f65aff6266
Beata Tomczak <tma.btty@gmail.com>
2025-08-25 21:41:30 +0200

    Display posts on feed page - functionality

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -1,16 +1,36 @@
 import React, { useEffect, useState } from 'react'
-import { assets, dummyPostsData } from '../assets/assets'
+import { useAuth } from '@clerk/clerk-react'
+import toast from 'react-hot-toast'
+
+import { assets } from '../assets/assets'
 import Loading from '../components/Loading'
 import StoriesBar from '../components/StoriesBar'
 import PostCard from '../components/PostCard'
 import RecentMessages from '../components/RecentMessages'
+import api from '../api/axios'
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([])
   const [loading, setLoading] = useState(true)
+  const { getToken } = useAuth()
 
   const fetchFeeds = async () => {
-    setFeeds(dummyPostsData)
+    try {
+      setLoading(true)
+      const { data } = await api.get('/api/post/feed', {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      })
+
+      if (data.success) {
+        setFeeds(data.posts)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
     setLoading(false)
   }
 
